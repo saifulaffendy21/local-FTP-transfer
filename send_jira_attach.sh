1ab57bd021689b1699370be2fba1e1cb7c740502
MOHAMAD SAIFUL AFFENDY BIN MOHD RIZAL <saifulaffendy21@gmail.com>
2019-05-21 09:41:19 +0800

    Update send_jira_attach.sh

--- send_jira_attach.sh
+++ send_jira_attach.sh
@@ -14,9 +14,10 @@ PASSWORD=<ENTER PASSWORD OF FTP USER>
 ZIPFILE="$(date +%d:%m:%Y-%H%M-attachments_jira.zip)"
 echo $ZIPFILE "will be ZIPPED" 
 # Here is where need to specify the directory of the file that need to be ZIP
-zip -r /var/log/FTP_BACKUP/$ZIPFILE /var/atlassian/application-data/jira/data/avatars/ /var/atlassian/application-data/jira/data/attachments/
-cd /var/log/FTP_BACKUP/
-
+cd /var/atlassian/application-data/jira/data/
+zip -r $ZIPFILE avatars/ attachments/
+# cd /var/log/FTP_BACKUP/
+echo ""
 echo "Connect FTP server to send the backup file"
 echo ""
 # Connect to FTP server
